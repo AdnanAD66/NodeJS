dfae90b7868141fa428b5a3145665ee4baad3830
AdnanAD66 <adnanadahmed764@gmail.com>
2025-08-02 09:14:27 +0500

    add, sub, multiply, divide in NodeJS CJS module

--- class_2/app.js
+++ class_2/app.js
@@ -2,10 +2,15 @@
 // const importFromsum = require('./sum') 
 
 // Destructuring
-const { subtract } = require('./sub')
 const { addition } = require('./sum') 
-
+const { subtract } = require('./sub')
+const { multiply } = require('./mul')
+const { divide } = require('./div')
 
 addition(2, 10)
 
 subtract(2, 2)
+
+multiply(2, 3)
+
+divide(10 , 3)
